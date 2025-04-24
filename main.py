1f1ac2fa3b65edd2c52a77799d10b22a88d54dd1
maggieley <maggasoarus@gmail.com>
2025-04-24 13:19:01 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import data
+from helpers import is_url_reachable
+
+class TestUrbanRoutes:
+    if is_url_reachable(data.URBAN_ROUTES_URL):
+        print("urban routes found")
+    else:
+        print("urban routes not found")
+
+def test_set_route():
+    # Add in S8
+    print("function created for set route")
+pass
+
+def test_select_plan():
+    # Add in S8
+    print("function created for select plan")
+pass
+
+def test_fill_phone_number():
+    # Add in S8
+    print("function created for fill phone number")
+pass
+
+def test_fill_card():
+    # Add in S8
+    print("function created for fill card")
+pass
+
+def test_comment_for_driver():
+    # Add in S8
+    print("function created for driver comment")
+pass
+
+def test_order_blanket_and_handkerchiefs():
+    # Add in S8
+    print("function created for blanket and hankerchiefs")
+pass
+
+def test_order_2_ice_creams():
+    # Add in S8
+    print("function created for 2 ice creams")
+pass
+
+def test_car_search_model_appears():
+    # Add in S8
+    print("function created for car search model")
+pass
